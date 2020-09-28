0831eb97c57ab069e0ac087158b9e1e11d89266e
trident6 <philip.johnson.it@gmail.com>
2020-09-28 09:35:25 -0400

    not valid yet

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log('Add validation!');
+
+// function validateForm() {
+//     var x = document.forms["myForm"]["fname"].value;
+//     if (x == "") {
+//         alert("Name must be filled out");
+//         return false;
+//     }
+// }
+
+
+
+
+// const form = document.querySelector('#parking-form')
+// let formIsValid
+
+// form.addEventListener('submit', function(event) {
+
+// })
+
+// function validateForm(event) {
+//     event.preventDefault()
+//     validateName()
+//     validateEmail()
+//     validateCar()
+
+
+// }
